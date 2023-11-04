9074ec038afbc55e469d796d9ca8f9942ef04972
antoniomtz12 <antonio.martinez.garcia@uabc.edu.mx>
2023-11-03 20:59:22 -0700

    Agregar archivos de proyecto.

---
+++ AMAZON1/Controllers/PRODUCTOController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMAZON1.Controllers
+{
+    public class PRODUCTOController : Controller
+    {
+        // GET: PRODUCTO
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AMAZON1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AMAZON1.Startup))]
+namespace AMAZON1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
